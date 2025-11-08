87d998412e9bfd52c28476218db74a8e81392cf2
fahadmemon1234 <fahadmemon131@gmail.com>
2025-11-08 17:14:58 +0500

    add file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,32 +22,32 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* <!-- Favicon --> */}
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="shortcut icon" href="/admin/favicon/favicon.ico" />
         {/* <!-- Bootstrap --> */}
-        <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/admin/assets/css/bootstrap.min.css" />
         {/* <!-- file upload --> */}
-        <link rel="stylesheet" href="/assets/css/file-upload.css" />
+        <link rel="stylesheet" href="/admin/assets/css/file-upload.css" />
         {/* <!-- file upload --> */}
-        <link rel="stylesheet" href="/assets/css/plyr.css" />
+        <link rel="stylesheet" href="/admin/assets/css/plyr.css" />
         {/* <!-- DataTables --> */}
         <link
           rel="stylesheet"
           href="https://cdn.datatables.net/2.0.8/css/dataTables.dataTables.min.css"
         />
         {/* <!-- full calendar --> */}
-        <link rel="stylesheet" href="/assets/css/full-calendar.css" />
+        <link rel="stylesheet" href="/admin/assets/css/full-calendar.css" />
         {/* <!-- jquery Ui --> */}
-        <link rel="stylesheet" href="/assets/css/jquery-ui.css" />
+        <link rel="stylesheet" href="/admin/assets/css/jquery-ui.css" />
         {/* <!-- editor quill Ui --> */}
-        <link rel="stylesheet" href="/assets/css/editor-quill.css" />
+        <link rel="stylesheet" href="/admin/assets/css/editor-quill.css" />
         {/* <!-- apex charts Css --> */}
-        <link rel="stylesheet" href="/assets/css/apexcharts.css" />
+        <link rel="stylesheet" href="/admin/assets/css/apexcharts.css" />
         {/* <!-- calendar Css --> */}
-        <link rel="stylesheet" href="/assets/css/calendar.css" />
+        <link rel="stylesheet" href="/admin/assets/css/calendar.css" />
         {/* <!-- jvector map Css --> */}
-        <link rel="stylesheet" href="/assets/css/jquery-jvectormap-2.0.5.css" />
+        <link rel="stylesheet" href="/admin/assets/css/jquery-jvectormap-2.0.5.css" />
         {/* <!-- Main css --> */}
-        <link rel="stylesheet" href="/assets/css/main.css" />
+        <link rel="stylesheet" href="/admin/assets/css/main.css" />
       </head>
       <body className={inter.className}>
         <div className="preloader">
@@ -60,29 +60,29 @@ export default function RootLayout({
         {children}
 
         {/* <!-- Jquery js --> */}
-        <Script src="/assets/js/jquery-3.7.1.min.js" />
+        <Script src="/admin/assets/js/jquery-3.7.1.min.js" />
         {/* <!-- Bootstrap Bundle Js --> */}
-        <Script src="/assets/js/boostrap.bundle.min.js" />
+        <Script src="/admin/assets/js/boostrap.bundle.min.js" />
         {/* <!-- Phosphor Js --> */}
-        <Script src="/assets/js/phosphor-icon.js" />
+        <Script src="/admin/assets/js/phosphor-icon.js" />
         {/* <!-- file upload --> */}
-        <Script src="/assets/js/file-upload.js" />
+        <Script src="/admin/assets/js/file-upload.js" />
         {/* <!-- file upload --> */}
         {/* <Script src="/assets/js/plyr.js" /> */}
         {/* <!-- dataTables --> */}
         <Script src="https://cdn.datatables.net/2.0.8/js/dataTables.min.js" />
         {/* <!-- full calendar --> */}
-        <Script src="/assets/js/full-calendar.js" />
+        <Script src="/admin/assets/js/full-calendar.js" />
         {/* <!-- jQuery UI --> */}
-        <Script src="/assets/js/jquery-ui.js" />
+        <Script src="/admin/assets/js/jquery-ui.js" />
         {/* <!-- jQuery UI --> */}
         {/* <Script src="/assets/js/editor-quill.js" /> */}
         {/* <!-- apex charts --> */}
-        <Script src="/assets/js/apexcharts.min.js" />
+        <Script src="/admin/assets/js/apexcharts.min.js" />
         {/* <!-- jvectormap Js --> */}
-        <Script src="/assets/js/jquery-jvectormap-2.0.5.min.js" />
+        <Script src="/admin/assets/js/jquery-jvectormap-2.0.5.min.js" />
         {/* <!-- jvectormap world Js --> */}
-        <Script src="/assets/js/jquery-jvectormap-world-mill-en.js" />
+        <Script src="/admin/assets/js/jquery-jvectormap-world-mill-en.js" />
 
         {/* <!-- main js --> */}
         <Script src="/assets/js/main.js" />
